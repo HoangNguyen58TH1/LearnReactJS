1d3b965d51a8bcc23e2e15be431437f3c5a9da64
HoangNguyen58TH1 <hoangnv20081998@gmail.com>
2021-01-20 15:37:11 +0700

    clear app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
 import './App.css';
-import TodoItem from './components/TodoItem';
 
 function App() {
-  const todoItems = [
-    { title: 10000, isCompleted: false },
-    { title: 20000, isCompleted: true },
-    { title: 30000, isCompleted: false }
-  ];
   return (
     <div className="App">
-      <header className="App-header">
-        {
-          todoItems.length > 0 && todoItems.map((item, index) =>
-            <TodoItem key={index} salary={item} />
-          )
-        }
-        { todoItems.length === 0 && 'Nothings here.' }
-      </header>
     </div>
   );
 }
