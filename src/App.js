ef8bc3ff0f1668fd3801736dc932fb6eb2f55381
HoangNguyen58TH1 <hoangnv20081998@gmail.com>
2021-01-20 09:43:29 +0700

    Done ways 2 task10. Learn add npm classNames

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import TodoItem from './components/TodoItem';
 
 function App() {
   const todoItems = [
     { title: 10000, isCompleted: false },
-    { title: 20000, isCompleted: false },
-    { title: 30000, isCompleted: true }
+    { title: 20000, isCompleted: true },
+    { title: 30000, isCompleted: false }
   ];
   return (
     <div className="App">
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,14 @@
 import './TodoItem.css'
+var classNames = require('classnames');
 
 function TodoItem(props) {
-  let className = 'TodoItem';
-  if (props.salary.isCompleted == true){
-    className += ' TodoItem-complete'
-  }
+  // let className = 'TodoItem';
+  // if (props.salary.isCompleted == true){
+    //   className += ' TodoItem-complete'
+    // }
+  var className = classNames('TodoItem',  {
+    'TodoItem-complete': props.salary.isCompleted
+  });
 
   return (
     <div className={className}>
