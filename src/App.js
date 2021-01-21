69283a5b814c4711247d5207096f19bee52a0652
HoangNguyen58TH1 <hoangnv20081998@gmail.com>
2021-01-22 00:18:53 +0700

    convert state from TrafficLight.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
+import React, { Component } from 'react';
 import './App.css';
 import TrafficLight from './components/TrafficLight';
 
-function App() {
-  return (
-    <div className="App">
-      <TrafficLight />
-    </div>
-  );
+const RED = 0;
+const YELLOW = 1;
+const GREEN = 2;
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { 
+      currentColor: RED
+    }
+
+    setInterval(() => {
+      this.setState({
+        currentColor: this.getNextColor(this.state.currentColor)
+      })
+    }, 1000)
+  }
+
+  getNextColor(color) {
+    switch(color) {
+      case RED:
+        return YELLOW;
+      case YELLOW:
+        return GREEN;
+      default:
+        return RED;
+    }
+  }
+
+  render() {
+    const { currentColor } = this.state;
+    return (
+      <div className="App">
+        <TrafficLight currentColor={ currentColor } />
+      </div>
+    );
+  }
 }
 
 export default App;
