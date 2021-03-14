50c1ea1387a672136efc1ac6aad0acc6a390844f
HoangNguyen58TH1 <hoangnv20081998@gmail.com>
2021-03-14 18:47:34 +0700

    show component App

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import App2 from './App2'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 // import App3 from './App3'
 // import App4 from './App4'
-import App5 from './App5'
-import App6 from './App6'
+// import App5 from './App5'
+// import App6 from './App6'
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
     {/* <App2 /> */}
     {/* <App3 /> */}
     {/* <App4 /> */}
-    <App5 />
+    {/* <App5 /> */}
     {/* <App6 /> */}
   </React.StrictMode>,
   document.getElementById('root')
